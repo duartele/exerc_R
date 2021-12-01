cdd8a28fd9a76aa66df2851ebad511e5cfd6f6b0
duartele <lndoxmat@hotmail.com>
2021-12-01 19:11:47 +0000

    Update Titanic.R

--- Titanic.R
+++ Titanic.R
@@ -1,4 +1,4 @@
-
+set.seed(17)
 setwd("C:/Users/lndox/OneDrive/Documentos/Mestrado Portugal/Computacao/Trabalho")
 df<-read.csv2("titanic.csv",colClasses = c("factor", "factor", "factor", "numeric",
                "integer", "integer", "numeric", "factor",
@@ -20,6 +20,20 @@ calc_faixa_etaria<-function(idade=NA){
 
 df["faixa_etaria"]<-sapply(df$age,FUN=calc_faixa_etaria)
 
+sozinho<-function(a){
+  if(a>0){
+    sozinho = 0
+    return(sozinho)
+  }else{
+    sozinho = 1
+    return(sozinho)
+  }
+}
+df["sozinho"]<-df$sibsp+df$parch
+df["sozinho"]<-sapply(df$sozinho,FUN=sozinho)
+
+summary(df)
+
 table(df$survived,df$faixa_etaria)
 
 boxplot(df$age~df$survived)
@@ -31,7 +45,9 @@ barplot(table(df$embarked,df$pclass), xlab="Porto de Embarque por Classe", ylab=
 
 boxplot(df$fare~df$pclass, ylab = "Valor da Tarifa", xlab="Classe",main="")
 
-
+sample<-sort(sample(nrow(df),nrow(df)*0.8))
+train<-df[sample,]
+val<-df[-sample,]
 
 df$fare<-ifelse(df$fare>50,50,df$fare)
 
@@ -187,149 +203,4 @@ windows()
 
 tab1<-table(pathscat,status)
 counts<-tab1
-barplot(counts,main="tpitulo")
-
-
-
-
-
-
-
-
-
-
-
-
-# Variáveis Qualitativas Ordinais
-Pclass (1, 2 ou 3)
-Das 1309 observações temos 323 da primeira (24,68%), 277 da segunda (21,16%) e 709 da terceira (54,16%) , sem valores faltantes, portanto. No gráfico a seguir, veremos para cada uma dessas categorias o comportamento da variável resposta.
-Aqui podemos ver que as pessoas da primeira classe tiveram mais chances de sobrevivência, seguindo pela segunda classe, tendo portanto a terceira classe com as maiores taxas de morte.
-
-# Variáveis Quantitativas Discreta
-Sibsp (Número de irmãos e ou esposos)
-Como podemos observar no gráfico, a maioria dos valores assume o valor 0 ou 1. Talvez podemos analisar esta variável agrupando em três categorias (0, 1 e maior que 1) - ou em duas("Não tem" ou "Tem").
-
-#Conclusão
-
-
-De acordo com o exposto, pessoas ricas e do sexo feminino tem mais chances de sobreviverem ao acidente do Titanic.
-
-#Rascunho
-
-+ **survived**: Sobrevivência (ou não) dos passageiros. Possíveis valores: 0 (morreu) ou 1 (sobreviveu). Classificação: Variável Resposta (de interesse). Quantidade de sobreviventes na amostra: `r table(df$survived)[2]` pessoas(`r round(table(df$survived)[2]/nrow(df)*100,2)`%);
-
-+ **pclass**: Classe do Bilhete. Possíveis valores: 1(primeira), 2(segunda) ou 3 (terceira). Classificação: Variável Qualitativa Ordinal
-Número de obs faltantes: `r  which(is.na(df$pclass));
-`
-+ **age**: Idade em anos. Possíveis valores: Para crianças menores de um ano, a idade pode assumir um valor contínuo (no intervalo  ]0,1[). Para mais velhos que isso, usa-se o valores discretos  {1,2,3...}. Quando tem o valor da forma xx.5 é porque a idade da pessoa foi estimada. Não obstante essas informações, podemos classificar a Variável como Quantitativa Contínua.
-Número de obs faltantes: `r  which(is.na(df$age));
-
-+ **sibsp**: Número de irmãos e/ou esposos a bordo. Possíveis valores: {0,1,2,3...}. Classificação: Variável Quantitativa Discreta.
-Número de obs faltantes: `r  which(is.na(df$sibsp));
-
-+ **parch**: Número de pais e/ou filhos a bordo. Possíveis valores: {0,1,2,3...}. Classificação: Variável Quantitativa Discreta.
-Número de obs faltantes: `r  which(is.na(df$parch));
-
-+ **fare**: Valor da tarifa paga. Possíveis valores: ]0,+???[ Classificação: Variável Quantitativa Contínua.
-                                                            Número de obs faltantes: `r  which(is.na(df$fare));
-                                                            
-                                                            + **embarked**: Porto de Embarque. Possíveis valores: C (Cherbourg), Q (Queenstown) ou S (Southampton). Classificação: Variável Qualitativa Nominal.
-                                                            Número de obs faltantes: `r  which(is.na(df$embarked));
-  
-                                                                                                                      
-                                                            + **death**: Morte (ou não) do passageiro. Possíveis valores: 0 (não morreu) ou 1(morreu). Esta variável será descartada porque foi obtida pela variável **survived**.
-                                                            
-                                                            
-                                                            Vale dizer, seria mais interessante se a variável sibsp fosse dividida em duas (uma só para irmãos e outra só para esposas). Nesse sentido, parch também poderia estar separada
-                                                            
-                                                            hist(df$parch)
-                                                            table(df$parch)
-df$embarked[df$embarked != "S"  "Q", "S")]                                                            
-split(df, df$embarked)
-df[169,]
-summary(df)
-?drop
-?remove
-
-
-cor(df$sibsp,df$parch)
-
-plot(df$sibsp~df$parch)
-abline(lm(df$sibsp~df$parch), col="red", lty=1, lwd=2)
-
-plot(df$sibsp~df$parch, xlab="Número de pais (ou filhos)", ylab="Número de irmãos (ou esposas)", main="")
-abline(lm(df$sibsp~df$parch), col="red", lty=1, lwd=2)
-
-
-boxplot(df$parch~df$faixa_etaria, ylab = "Némero de pais (ou filhos)", xlab="Faixa etária",main="")
-
-sozinho<-function(a){
-  
-  if(a>0){
-    sozinho = 0
-    return(sozinho)
-  }else{
-    sozinho = 1
-    return(sozinho)
-  }
-}
-sozinho(0,0)
-sozinho(1,0)
-sozinho(NA,1)
-
-df["sozinho"]<-df$sibsp+df$parch
-df["sozinho"]<-sapply(df$sozinho,FUN=sozinho)
-
-lot(df$sibsp~df$parch, col=df$sozinho)
-table(df$sozinho)
-warnings()
-calc_faixa_etaria<-function(idade=NA){
-  if(is.na(idade)){
-    faixa_etaria = "3.Sem idade informada"
-    return(faixa_etaria)
-  }else if(idade<=15){
-    faixa_etaria = "1.Crianças e jovens"
-    return(faixa_etaria)
-  } else{
-    faixa_etaria = "2.Adultos"
-    return(faixa_etaria)
-  }
-}
-
-df["faixa_etaria"]<-sapply(df$age,FUN=calc_faixa_etaria)
-table(df$faixa_etaria)
-
-do_mau<-function(sibsp=0, parch=0){
-  if(is.na(sibsp)){sibsp=0}
-  if(is.na(parch)){parch=0}
-  if(sibsp+parch>0){
-    sozinho = 0
-    return(sozinho)
-  }else{
-    sozinho = 1
-    return(sozinho)
-  }
-}
-
-df["sozinho"]<-sapply(df$sibsp+df$parch,FUN=do_mau)
-
-p_est<- table(df$survived)[2]/nrow(df)
-
-sd <- sqrt(p_est*(1-p_est)/nrow(df))
-
-LI <- p_est - qnorm(0.975)*sd
-LS <- p_est + qnorm(0.975)*sd
-
-table(df$survived,df$sex)[2,2]/table(df$sex)[2]
-table(df$sex)[2]
-
-sd <- sqrt(p_est*(1-p_est)/nrow(df))
-
-LI <- p_est - qnorm(0.975)*sd
-LS <- p_est + qnorm(0.975)*sd
-
-p_xx<-table(df$survived,df$sex)[2,1]/table(df$sex)[1]
-p_xy<-table(df$survived,df$sex)[2,2]/table(df$sex)[2]  
-
-se <- sqrt(p_xx*(1-p_xx)/table(df$sex)[1] + p_xy*(1-p_xy)/table(df$sex)[1]) 
-p_xx-p_xy+c(-1,1)*qnorm(0.975)*se
+barplot(counts,main="titulo")
