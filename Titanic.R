16c44695225556658d78fdb656ba916b3dfa74d6
duartele <lndoxmat@hotmail.com>
2021-12-04 22:53:10 +0000

    Update Titanic.R

--- Titanic.R
+++ Titanic.R
@@ -37,7 +37,7 @@ df$faixa_etaria<-factor(df$faixa_etaria)
 df$sozinho<-factor(df$sozinho)
 df$age[is.na(df$age)]<-mean(df$age,na.rm = TRUE)
 df$fare[is.na(df$fare)]<-mean(df$fare,na.rm = TRUE)
-df$embarked[df$embarked==""]<-"S" 
+#df$embarked[df$embarked==""]<-"S" 
 df$embarked<-factor(df$embarked)
 
 library(caret)
@@ -108,33 +108,13 @@ X_train<-df[sample,]
 X_val<-df[-sample,]
 y_train<-target[sample,]
 y_val<-target[-sample,]
-
-
-
-
-
 str(df)
 table(df$pclass)
-
-
-
 
 summary(df)
 df$embarked[df$embarked==""]<-"S"
 which(is.na(df$age))
 sum(is.na(df$fare))
-
-table(df$survived,df$embarked)
-pie(x=table(df$embarked))
-boxplot(df$age~df$survived)
-
-boxplot(df$fare~df$survived, ylab = "Valor da Tarifa", xlab="Sobreviv?ncia",main="")
-
-barplot(table(df$embarked,df$pclass), xlab="Porto de Embarque por Classe", ylab="Frequ?ncia",
-        legend.text = FALSE, beside=FALSE)
-
-boxplot(df$fare~df$pclass, ylab = "Valor da Tarifa", xlab="Classe",main="")
-
 sample<-sort(sample(nrow(df),nrow(df)*0.8))
 train<-df[sample,]
 val<-df[-sample,]
@@ -147,159 +127,57 @@ for (val in df$fare){
   df$fare <- 300
 }
 } 
-df$fare[df$fare>300]
-
-df<- subset(df, select = -death)
-summary(df)
-?subset
-barplot(table(df$survived,df$sex),beside =TRUE,legend=c("Morreu","Viveu"))
-barplot(table(df$survived,df$sex))
-legend("topleft",legend=c("Morreu","Viveu"))
-?legend
-?barplot
-library(lattice)
-which(is.na(df$survived))
-?theme()
-pie(x=table(pathscat), labels=c("<2cm","2-5 cm",">5 cm")
-    , col = c("lightblue", "lightgreen", "lightgrey"))
-
-cor(df$sibsp,df$parch)
-?cor
-
-
-
-
-?hist
-min(df$age,na.rm=TRUE)
-max(df$age)
-hist(df$age, 
-     breaks=c(min(df$age,na.rm=TRUE),35,60,69,max(df$age,na.rm=TRUE)),
-       main="Idade", xlab="Idade",
-     
-     freq=TRUE,ylab="frequ?ncias",labels = FALSE, col="blue"
-     )
-windows()
-
-jpeg(filename = "histograma.jpeg")
-hist(df$age, 
-     main="Idade", xlab="Idade",
-     
-     freq=TRUE,ylab="frequ?ncias",labels = FALSE, col="blue"
-     )
-curve(dnorm(x, mean = mean(df$age,na.rm = T), sd = sd(df$age, na.rm =T)), add = T)   
-dev.off()
-?dnorm()
-
-hist(df$age, 
-     main="Idade", xlab="Idade",
-     
-     freq=FALSE,ylab="frequ?ncias",labels = FALSE, col="blue"
-)
-curve(dnorm(x, mean = mean(df$age,na.rm = T), sd = sd(df$age, na.rm =T)), add = T)   
-legend("bottomright",legend = c("fcom","norm"),text.col = 1:2)
-locator(1)
-
-legend(x=c(60,80),y=c(160,200),legend = c("fcom","norm"),text.col = 1:2)
-
-
-
-?read.csv2
-is.num
-is.numeric(df$fare)
-500/(500+809)
-vivos<- df$death[df$survived==0]
-table(vivos) 
-table(df$pclass)/length(df$pclass)
-843/(500+809)
-?barplot
-main="GR?FICO"
-?barplot
-a<-table(df$survived,df$sex)
-b<-table(df$survived)
-b
-names(b)<-c("sobreviveu","morreu")
-a
-table(df$survived)
-barplot(a,legend.text = TRUE)
-barplot(b)
-windows()
-barplot(table(df$survived,df$sex),legend.text = TRUE)
-
-legend("bottomright", legend=c("sobreviveu", "morreu"))
-legend(x=3,y=1000, legend=c("sobreviveu", "morreu"))
-legend("topleft", inset = 0.50, legend=c(1, 0))
-
-
-barplot(cbind(Employed, Unemployed) ~ Year, data = longley ,legend.text = TRUE)
-longley
-?legend
-
-barplot(table(mtcars$gear), col = 2:4, density = 30, angle = 90)
-
-legend("topright",
-       legend = c(3, 4, 5),
-       fill = 2:4,
-       density = 30, # Shading lines density
-       angle = 90)   # Angle of the shading lines
-
-
-?plot
-?legend.text
-head(df$sex)
-pie(df$sex)
-str(df)
-table(df$sex)
-barplot(frequency(df$sex))
-?mfrow
-barplot(table(df$survived,df$pclass),legend.text = TRUE)
-table(df$survived,df$pclass)/length(df$survived)
-123/(123+200)
-158/(158+119)
-528/(528+181)
-hist(table(df$survived,df$sibsp),legend.text = TRUE)
-summary(df$sibsp)
-hist(df$sibsp)
-table(df$sibsp)/length(df$sibsp)
-
-# Create a sample of 50 numbers which are incremented by 1.
-x <- seq(0,50,by = 1)
-
-# Create the binomial distribution.
-y <- dbinom(x,50,0.2)
-
-# Give the chart file a name.
-png(file = "dbinom.png")
-
-# Plot the graph for this sample.
-plot(x,y)
-
-pie(table(df$survived),labels = c("morreu","viveu"))
-pie.table<-table(df$survived)
-names(pie.table)<-c("morreu", "viveu")
-pie.table
-pie(pie.table)
-pie(c(10,20,30,40))
-pie(c(10,20,30,40),labels=c("Grupo I", "Grupo II", "Grupo III","Grupo IV"))
-
-
-col.pie <- c("red", "yellow", "green", "violet")
-label.pie <- c ("Grupo I", "Grupo II", "Grupo III","Grupo IV")
-freq <- round(c(10,20,30,40)/ sum(c(10,20,30,40)), 3)*100
-label.pie <- paste(label.pie, freq)
-label.pie
-label.pie <- paste(label.pie,"%",sep="")
-pie(freq, col = col.pie, labels = label.pie)
-windows()
-
-tab1<-table(pathscat,status)
-counts<-tab1
-barplot(counts,main="titulo")
 
 normalize<-function(x){
   return((x-min(x))/(max(x)-min(x)))
 }
 df_norm<-as.data.frame(lapply(df, normalize))
+
 require(class)
 mi<-knn(train = df, test = df_, cl=target, k=13)
 table(target,m1)
 summary(df_norm)
+
+normalize<-function(x) return((x-min(x))/(max(x)-min(x)))
+#Aplicando a normalização na base:
+df_norm<-as.data.frame(lapply(df, normalize))
+X_train<-df_norm[sample,] #seed(17) já foi fixado
+X_val<-df_norm[-sample,]
+y_train<-target[sample,]
+y_val<-target[-sample,]
+l_acc<-c()#inicializando o vetor de acc
+for( i in 1:40){
+  m1<-knn(train = X_train, test = X_val, cl=y_train, k=i)
+  acc<-sum(table(m1,y_val)[1,1],table(m1,y_val)[2,2])/length(y_val)
+  l_acc<-append(l_acc,acc)
+} 
+library(FSelector)
+library(rpart)
+library(rpart.plot)
+library(dplyr)
+library(data.tree)
+
+#Decision Tree
+library(rpart)
+library(rpart.plot)
+
+train<-subset(train, select=-age)
+
+tree<-rpart(survived ~., data=train)
+prp(tree)
+pred<-predict(tree, val, type="class")
+table(pred,val$survived)
+acc<-sum(table(pred,val$survived)[1,1],table(pred,val$survived)[2,2])/length(pred)
+
+
+#tree<-rpart(survived ~., data=X_train, method="anova") - target is nummeric
+pred<-predict(tree, X_val, type="class")
+acc<-sum(table(pred,y_val)[1,1],table(pred,y_val)[2,2])/length(y_val)
+prp(tree)
+
+#for nummeric response
+MAE<-function(actual, pred){
+  mean(abs(actual-pred))
+}
+MAE(y_val, pred)
+rpart.plot(m1, type=3, digits=3, fallen.leaves=TRUE)
